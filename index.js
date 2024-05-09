9f51ca0ff8555de6c72e302762eb2dc6e2fe76a6
moony <moonysl@hanmail.net>
2024-05-10 06:48:06 +0900

    io connection 확인

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-app.use('/', express.static('public'))
+const io = require('socket.io')(http);
+
+app.use('/', express.static('public'));
+app.use('/socket.io', express.static('node_modules/socket.io/client-dist'))
+
+io.on('connection', socket => {
+    console.log("connection");
+});
 
 http.listen(3000, () => {
     console.log('listening on localhost:3000');
---
+++ public/main.js
@@ -0,0 +1,1 @@
+socket = io(null, { transports: ['websocket'] });
